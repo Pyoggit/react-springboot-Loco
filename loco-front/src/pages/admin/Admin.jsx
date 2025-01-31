7bad11fd25c9e3b00c0e2a6b09e20cb8e2587a9c
jangpyo <wkdvy122851@gmail.com>
2025-01-31 14:11:57 +0900

    [CREATE] 관리자모드 페이지 렌더링

---
+++ loco-front/src/pages/admin/Admin.jsx
@@ -0,0 +1,12 @@
+import { Route, Routes } from "react-router-dom";
+
+const Admin = () => {
+  return (
+    <>
+      <h1>관리자모드 렌더링 할 페이지</h1>
+      <Routes></Routes>
+    </>
+  );
+};
+
+export default Admin;
