fc1979093c60a657a1a203d650fc6ab969d8f958
jangpyo <wkdvy122851@gmail.com>
2025-01-31 14:15:25 +0900

    [CREATE] 전체 페이지 경로 설정

---
+++ loco-front/src/common/index.js
@@ -0,0 +1,4 @@
+export const HOME_PATH = () => "/";
+export const LOGIN_PATH = () => "/member/Sign/LoginMain";
+export const SEARCH_PATH = (searchWord) => `/search/${searchWord}`;
+export const USER_PATH = (userEmail) => `/user/${userEmail}`;
