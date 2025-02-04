6c69db072a942858ba9e3ab264ce9cff40b8555e
jangpyo <wkdvy122851@gmail.com>
2025-02-04 10:09:53 +0900

    [CREATE] 각자 페이지 분배

---
+++ loco-front/src/pages/member/BoardPage.jsx
@@ -0,0 +1,7 @@
+export default function BoardPage() {
+  return (
+    <>
+      <h1>연아가 짜야할 자유게시판페이지 라우터 경로</h1>
+    </>
+  );
+}
---
+++ loco-front/src/pages/member/ServicePage.jsx
@@ -0,0 +1,7 @@
+export default function ServicePage() {
+  return (
+    <>
+      <h1>고객센터페이지 라우터 경로(FAQ,문의,신고)</h1>
+    </>
+  );
+}
