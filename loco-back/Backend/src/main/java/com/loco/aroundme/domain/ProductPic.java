b269f5da6f96b97c4a301ba0616eaeca26ea2434
minsung <minsung21c@gmail.com>
2025-02-13 10:19:40 +0900

    [CREATE] 상품페이지 백엔드 기능 생성

---
+++ loco-back/Backend/src/main/java/com/loco/aroundme/domain/ProductPic.java
@@ -0,0 +1,10 @@
+package com.loco.aroundme.domain;
+
+import java.sql.Timestamp;
+
+public class ProductPic {
+	private int pictureId;
+    private int productId;
+    private String pictureUrl;
+    private Timestamp pictureRegdate;
+}
