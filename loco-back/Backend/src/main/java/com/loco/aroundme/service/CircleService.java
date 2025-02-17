03bd3a5ba7dc12e901b3a742c971c8d0497a65a0
njh99 <whdgh1117@naver.com>
2025-02-17 10:37:56 +0900

    Circle 모임참가부분 추가 및 모임생성부분 수정

--- loco-back/Backend/src/main/java/com/loco/aroundme/service/CircleService.java
+++ loco-back/Backend/src/main/java/com/loco/aroundme/service/CircleService.java
@@ -3,7 +3,11 @@ package com.loco.aroundme.service;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
+
+import com.loco.aroundme.common.security.domain.CustomUser;
 import com.loco.aroundme.domain.Circle;
+import com.loco.aroundme.domain.Users;
 
 public interface CircleService {
 	List<Circle> getCirclesByDate(String date);
@@ -14,9 +18,16 @@ public interface CircleService {
 
 	void deleteCircle(Long circleId);
 
-	boolean attendCircle(Long circleId);
 
 	Circle findCircleById(int circleId); // 기존 getCircleById에서 변경
 
 	void updateCircle(Circle circle);
+	
+	void attendCircle(int circleId, int userId);
+
+	void cancelAttendance(int circleId, int userId);
+
+	List<Users> getAttendeesByCircleId(int circleId);
+
+	boolean isUserAttending(int circleId, int userId);
 }
