d1cd0532055718383d0264d7ed784aa6efe092c8
jangpyo <wkdvy122851@gmail.com>
2025-02-17 10:42:48 +0900

    [UPDATE] Router 경로 합치기

--- loco-front/src/App.jsx
+++ loco-front/src/App.jsx
@@ -5,13 +5,17 @@ import AdminLoginMain from "./components/admin/AdminLoginMain";
 import LoginMain from "./components/member/Sign/LoginForm";
 import FindEmailForm from "./components/member/Sign/FindEmailForm";
 import FindPwForm from "./components/member/Sign/FindPwForm";
+import FindPwVerify from "./components/member/Sign/FindPwVerify";
+import FindPwResult from "./components/member/Sign/FindPwResult";
 import SendPwResult from "./components/member/Sign/SendPwResult";
 import KakaoCallback from "./components/member/Sign/KakaoCallback";
 import SignUp from "./components/member/Sign/SignUpForm";
 import ModifyMember from "./components/member/Mypage/ModifyMember";
 import DeleteMember from "./components/member/Mypage/DeleteMember";
+import FindEmailResult from "./components/member/Sign/FindEmailResult";
 import MypageMain from "./components/member/Mypage/MypageMain";
 import Admin from "./pages/admin/Admin";
+import ResignKakaoUser from "./components/member/Sign/ResignKakaoUser";
 
 function App() {
   return (
@@ -22,9 +26,13 @@ function App() {
       <Route path="/login" element={<LoginMain />} />
       <Route path="/signup" element={<SignUp />} />
 
+      <Route path="/resign-kakao" element={<ResignKakaoUser />} />
+
       <Route path="/find/email" element={<FindEmailForm />} />
-      <Route path="/find/pw" element={<FindPwForm />} />
-      <Route path="/send/pw/result" element={<SendPwResult />} />
+      <Route path="/find/email/result" element={<FindEmailResult />} />
+      <Route path="/find-password" element={<FindPwForm />} />
+      <Route path="/find-password/verify" element={<FindPwVerify />} />
+      <Route path="/find-password/result" element={<FindPwResult />} />
 
       {/* 카카오로그인 콜백처리 */}
       <Route path="/kakao-callback" element={<KakaoCallback />} />
