d868050f8827a82618d33470183649ef13b95700
jangpyo <wkdvy122851@gmail.com>
2025-02-19 11:43:09 +0900

    [UPDATE] 관리자 인증 토큰 수정

--- loco-front/src/utils/AxiosConfig.jsx
+++ loco-front/src/utils/AxiosConfig.jsx
@@ -1,9 +1,13 @@
 import axios from "axios";
+
 axios.defaults.withCredentials = true;
 
 const instance = axios.create({
-  baseURL: "http://localhost:8080/",
-  withCredentials: true, // ✅ 쿠키 포함 요청
+  baseURL: "http://localhost:8080", // ✅ 프론트엔드가 아니라 백엔드로 요청을 보냄
+  withCredentials: true, // ✅ 쿠키 및 인증 헤더 포함
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default instance;
