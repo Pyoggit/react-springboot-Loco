377e20aced84659cf4bac4c17dd17ebda67873f2
jangpyo <wkdvy122851@gmail.com>
2025-02-21 15:30:12 +0900

    [UPDATE] 관리자 페이지 수정

--- loco-front/src/components/admin/AdminAside.jsx
+++ loco-front/src/components/admin/AdminAside.jsx
@@ -7,15 +7,12 @@ const AdminAside = ({ setSelectedPage }) => {
       <ul>
         <li onClick={() => setSelectedPage("dashboard")}>대시보드</li>
         <li onClick={() => setSelectedPage("members")}>회원 관리</li>
-        {/* <li onClick={() => setSelectedPage("intro")}>페이지 소개 관리</li> */}
-        <li onClick={() => setSelectedPage("notice")}>공지사항 관리</li>
-        <li onClick={() => setSelectedPage("freeboard")}>자유게시판 관리</li>
+        <li onClick={() => setSelectedPage("freeboard")}>게시판 관리</li>
+        <li onClick={() => setSelectedPage("notice")}>댓글 관리</li>
         <li onClick={() => setSelectedPage("faq")}>FAQ 관리</li>
-        <li onClick={() => setSelectedPage("qna")}>Q&A 관리</li>
         <li onClick={() => setSelectedPage("circles")}>모임/참여 관리</li>
         <li onClick={() => setSelectedPage("market")}>중고거래 상품 관리</li>
         <li onClick={() => setSelectedPage("pay")}>매출 & 정산 관리</li>
-        <li onClick={() => setSelectedPage("customer")}>고객센터 관리</li>
       </ul>
     </aside>
   );
