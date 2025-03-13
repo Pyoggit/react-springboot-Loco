4a8a32c764c4bb405a3aff468ecd8290b0b8174a
jangpyo <wkdvy122851@gmail.com>
2025-03-13 22:59:20 +0900

    [UPDATE] API 요청 시 저장된 JWT 토큰을 헤더에 자동으로 추가

--- loco-front/src/utils/AxiosConfig.jsx
+++ loco-front/src/utils/AxiosConfig.jsx
@@ -1,39 +1,17 @@
-// import axios from "axios";
-
-// axios.defaults.withCredentials = true;
-
-// const instance = axios.create({
-//   baseURL: "http://localhost:8080",
-//   withCredentials: true, // ✅ 쿠키 및 인증 헤더 포함
-//   headers: {
-//     "Content-Type": "application/json",
-//   },
-// });
-
-// instance.interceptors.request.use((config) => {
-//   const token = localStorage.getItem("normal_accessToken");
-//   if (token) {
-//     config.headers.Authorization = `Bearer ${token}`;
-//   }
-//   return config;
-// });
-
-// export default instance;
-
 import axios from "axios";
 
-axios.defaults.withCredentials = true;
+axios.defaults.withCredentials = true; // ✅ CORS 요청 시 쿠키 포함
 
 const instance = axios.create({
   baseURL: "http://localhost:8080",
-  withCredentials: true, // ✅ 쿠키 및 인증 헤더 포함
+  withCredentials: true, // ✅ JWT 쿠키 및 인증 헤더 포함
   headers: {
     "Content-Type": "application/json",
   },
 });
 
+// ✅ 요청 인터셉터: Authorization 헤더 자동 추가
 instance.interceptors.request.use((config) => {
-  // 이미 Authorization 헤더가 있으면 변경하지 않음
   if (!config.headers.Authorization) {
     const token = localStorage.getItem("normal_accessToken");
     if (token) {
